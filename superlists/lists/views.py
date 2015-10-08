a43191ae4e83603b60c045264f0fce4c72086b1c
Greg DeLozier <gregdelozier@gmail.com>
2015-10-07 20:52:38 -0400

    Updates for post and FT

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 
 def home_page(request):
     #return HttpResponse('<html><title>To-Do Lists</title></html>')
-    return render(request, 'home.html')
-    pass
-	
+    #if request.method == 'POST':
+    #    return 
+    #return render(request, 'home.html')
+    return render(request, 'home.html', {
+            'new_item_text': request.POST.get('item_text', '')                        
+        })
